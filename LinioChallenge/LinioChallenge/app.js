2ab9491bff7f6a4c207912f14604de7c836c22a6
poluxms <nicolas.meneses07@hotmail.com>
2020-02-23 19:28:59 -0300

    UPDATE Files

--- LinioChallenge/LinioChallenge/app.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const GeneradorNumeros_1 = require("./src/GeneradorNumeros");
-console.log(GeneradorNumeros_1.GenerarNumeros(1, 100));
-//# sourceMappingURL=app.js.map
